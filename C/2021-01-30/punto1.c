60b6bc4d29ccf176e7d7ea4f8d551d9acbd4fd4a
esposm03 <36164633+esposm03@users.noreply.github.com>
2021-01-30 18:09:09 +0100

    Esercizi di C

---
+++ C/2021-01-30/punto1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    float debito;
+    float interesse;
+    float importo_rata;
+
+    printf("Inserire il debito iniziale: ");
+    scanf("%f", &debito);
+
+    printf("Inserire l'interesse: ");
+    scanf("%f", &interesse);
+
+    printf("Inserire l'importo delle rate: ");
+    scanf("%f", &importo_rata);
+
+    interesse /= 100;
+
+    if (debito * interesse >= importo_rata) {
+        printf("Attenzione, la rata annuale e' troppo bassa per estinguere il debito\n");
+        return 1;
+    }
+
+    int i = 0;
+    while (debito > 0) {
+        debito += debito * interesse;
+        debito -= importo_rata;
+        i++;
+    }
+
+    if (i == 1) {
+        printf("Per estinguere il debito e' necessaria una sola rata\n");
+    } else {
+        printf("Per estinguere il debito sono necessarie %d rate\n", i);
+    }
+}
---
+++ C/2021-01-30/punto2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void piano(float *superficie, float *volume) {
+    int num_stanze;
+    float h;
+
+    printf("Inserire l'altezza del piano: ");
+    scanf("%f", &h);
+    printf("Inserire il numero di stanze nel piano: ");
+    scanf("%d", &num_stanze);
+
+    for (int i = 0; i < num_stanze; i++) {
+        float b, l;
+
+        printf("Base della stanza: ");
+        scanf("%f", &b);
+        printf("Lunghezza della stanza: ");
+        scanf("%f", &l);
+
+        *superficie += b * l;
+        *volume += b * l * h;
+    }
+}
+
+int main() {
+    int num_piani;
+    printf("Inserire il numero di piani: ");
+    scanf("%d", &num_piani);
+
+    float sup = 0;
+    float vol = 0;
+
+    for (int i = 0; i < num_piani; i++) {
+        piano(&sup, &vol);
+    }
+
+    printf("Superficie: %f m^2, volume: %f m^2\n", sup/10000, vol/10000);
+}
